2eee4d4f156fe2193df00d0489f41d80b084f4f8
CharlieRojas <crc_185@yahoo.com>
2022-03-22 00:09:28 -0500

    commit tree e2a8a9067ca85468e80e80f1324ba6e239c6a173

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+const formData = {
+    name: "Steve",
+    email: "steve@steve.com",
+};
+
+const init =  {
+method:'POST',
+headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+},
+body: JSON.stringify(formData),
+};
+
+function submitData(Name,email) {
+    return fetch('http://localhost:3000/users', init)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (object) {
+            document.body.append(object.id);
+        })
+        .catch(function (error) {
+            alert("Invalid Name-Email combination!");
+            document.body.append(error.message);
+        });
+};
+submitData('charlie','ye.com');
